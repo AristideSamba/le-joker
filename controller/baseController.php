cce1aa809051535225caac6c511c34ad1f24db5f
AristideSamba <sambaaristide@gmail.com>
2024-10-03 16:35:43 +0100

    latest

--- controller/baseController.php
+++ controller/baseController.php
@@ -5,8 +5,7 @@
     $styleCss = "public/css/style.css";
     $_date = date('Y-d-m');
     $_years = new DateTime();
-    $navigator = $_SERVER["PHP-SELF"];
-    $
+    
 
     # classe ref
     class User{
@@ -17,3 +16,50 @@
     # instance
     $newtitle = new User();
     
+   
+                    class Utilisateur{
+                        public function actorsInfo(){
+                            $actors = [
+                                array(
+                                    "prenom"=>"Joaquin",
+                                    "nom"=>"Phoenix",
+                                    "role"=>"dans le rôle d'Arthur Fleck (Joker)"
+                                ),
+                                array(
+                                    "prenom"=>"Robert",
+                                    "nom"=>"De Niro",
+                                    "role"=>"dans le rôle de Murray Franklin"
+                                ),
+                                array(
+                                    "prenom"=>"Zazie",
+                                    "nom"=>"Beetz",
+                                    "role"=>"dans le rôle de Sophie Dumond"
+                                ),
+                                array(
+                                    "prenom"=>"Frances",
+                                    "nom"=>"Conroy",
+                                    "role"=>"dans le rôle de Penny Fleck"
+                                ),
+                                
+                            ];
+                        
+                        return $actors;
+                    }
+                        public function storyline(){
+                            $story = ["title"=>"Histoire du film", 
+                            "storyline" => "En 1981, à Gotham City, Arthur Fleck, un comédien raté, sombre dans la folie
+                            après une série de tragédies et d'humiliations. Incapable de se connecter aux
+                            autres et désespéré par l'absence de sens dans sa vie, Arthur se transforme
+                            progressivement en Joker, une figure de chaos et de violence, qui devient 
+                            une icône pour les citoyens les plus marginalisés."];
+                            return $story;
+                        }
+                }
+                $instance_utilisateur = new Utilisateur();
+                $actors = $instance_utilisateur->actorsInfo();
+
+
+                
+                
+                
+                
--- index.php
+++ index.php
@@ -19,25 +19,28 @@
 
         <!-- Section Storyline -->
         <section class="storyline">
-            <h2>Histoire</h2>
+            <?php
+             $story = $instance_utilisateur->storyline(); 
+             ?>
+            <h2><?= $story['title'] ?></h2>
             <p>
-                En 1981, à Gotham City, Arthur Fleck, un comédien raté, sombre dans la folie
-                après une série de tragédies et d'humiliations. Incapable de se connecter aux
-                autres et désespéré par l'absence de sens dans sa vie, Arthur se transforme
-                progressivement en Joker, une figure de chaos et de violence, qui devient 
-                une icône pour les citoyens les plus marginalisés.
+                <?= $story['storyline'] ?>
             </p>
         </section>
 
         <!-- Section Cast -->
         <section class="cast">
             <h2>Cast Principal</h2>
+            <?php foreach($actors as $role):?>
             <ul>
-                <li><strong>Joaquin Phoenix </strong>dans le rôle d'Arthur Fleck (Joker)</li>
-                <li><strong>Robert De Niro</strong> dans le rôle de Murray Franklin</li>
-                <li><strong>Zazie Beetz</strong> dans le rôle de Sophie Dumond</li>
-                <li><strong>Frances Conroy</strong> dans le rôle de Penny Fleck</li>
+                
+                <li>
+                    <strong><?= $role['prenom']." ".$role['nom'] ?>
+                    </strong> <?= $role['role'] ?>
+                </li>
+             
             </ul>
+            <?php endforeach;?>
         </section>
 
         <!-- Section Date de Sortie -->
@@ -61,11 +64,11 @@
         include_once __DIR__ . '/template/footer.inc.php'
     ?>
 
-    <!-- <pre>
+    <pre>
         <?php 
-           # print_r($_SERVER)
+           print_r($_SERVER)
         ?>
-    </pre> -->
+    </pre>
 
 </body>
 </html>
